e7491f9085fe3d9d356b17d4ebfdec3bd9b29b7b
Rakesh21M <rockmvijay@gmail.com>
2019-08-19 12:27:07 +0530

    master msg

---
+++ ChallengeConcurrency2/problem1.go
@@ -0,0 +1,45 @@
+// This is the example of Fan-Out
+
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+var workerID int
+var publisherID int
+
+func main() {
+	input := make(chan string)
+	go workerProcess(input)
+	go workerProcess(input)
+	go workerProcess(input)
+	go publisher(input)
+	go publisher(input)
+	go publisher(input)
+	go publisher(input)
+	time.Sleep(10 * time.Millisecond)
+}
+
+func publisher(out chan string) {
+	publisherID++
+	thisID := publisherID
+	dataID := 0
+	for {
+		dataID++
+		fmt.Printf("publisher %d is pushing data\n", thisID)
+		data := fmt.Sprintf("data from publisher %d. Data %d", thisID, dataID)
+		out <- data
+	}
+}
+
+func workerProcess(in <-chan string) {
+	workerID++
+	thisID := workerID
+	for {
+		fmt.Printf("%d: waiting for input ...\n", thisID)
+		input := <-in
+		fmt.Printf("%d, Input is : %s\n", thisID, input)
+	}
+}

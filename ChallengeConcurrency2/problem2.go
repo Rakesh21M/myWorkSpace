7e455fba4af66a3c0ef3a8cc7efdacf8e1d62601
Rakesh21M <rockmvijay@gmail.com>
2019-08-20 08:49:11 +0530

    master msg

---
+++ ChallengeConcurrency2/problem2.go
@@ -0,0 +1,49 @@
+// execute 1000 factorial computation concurrently
+// How much your resources being used
+
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	in := gen()
+	f := factorial(in)
+
+	for n := range f {
+		fmt.Println(n)
+	}
+}
+
+func gen() <-chan int {
+	out := make(chan int)
+	go func() {
+		for i := 0; i < 1; i++ {
+			for j := 3; j < 13; j++ {
+				out <- j
+			}
+		}
+		close(out)
+	}()
+	return out
+}
+
+func factorial(in <-chan int) <-chan int {
+	out := make(chan int)
+	go func() {
+		for n := range in {
+			out <- fact(n)
+		}
+		close(out)
+	}()
+	return out
+}
+
+func fact(n int) int {
+	total := 1
+	for i := n; i > 0; i++ {
+		total *= i
+	}
+	return total
+}
